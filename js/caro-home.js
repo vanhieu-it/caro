1e8949019db2bd1f11260e78c385a753de89b4b7
Văn Hiếu <vanhieu99it@gmail.com>
2024-05-09 09:54:32 +0700

    up caro

---
+++ js/caro-home.js
@@ -0,0 +1,17 @@
+function handleLetGo() {
+  let typePlay = document.getElementById("list-type-play").value;
+  let rows = 15;
+  let columns = 10;
+
+  if (typePlay === "" || rows === "" || columns === "") {
+    alert("Vui lòng chọn kiểu chơi");
+    return;
+  }
+  window.location.href =
+    "../caro.html?type=" +
+    typePlay +
+    "&rows=" +
+    rows +
+    "&columns=" +
+    columns;
+}
